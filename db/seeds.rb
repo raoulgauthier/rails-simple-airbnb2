c2b0e4130e214efaeb938bf7613f8b0246d0956c
Raoul Gauthier <raoul_gauthier@outlook.com>
2018-11-01 09:24:07 +0100

    trial aibnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:
+
+puts 'Creating flats...'
+Flat.destroy_all
+
+flats = Flat.create!([
+  { name: 'Spacious Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75,  number_of_guests: 3 },
+  { name: 'Big Beach Miami House', address: '95 sentier du lièvre', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75,  number_of_guests: 3 },
+  { name: 'Tower In Central Paris', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75,  number_of_guests: 3 },
+  { name: 'Little Cabin In The Forest', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75,  number_of_guests: 3 },
+  { name: 'Lovely Van Near Mountains', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75,  number_of_guests: 3 },
+ ])
